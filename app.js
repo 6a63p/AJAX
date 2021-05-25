bf827a30aefc325a0f6ccf23a31b33e1b57abac9
6a63p <fullname@iMac6a63p.local>
2021-05-25 10:56:44 +0300

    eslint

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
 const express = require('express');
-const app = express()
+
+const app = express();
 const bodyParser = require('body-parser');
 
-module.exports = app 
+module.exports = app;
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: true}))
-
-
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-app.get('/', (req, res) => res.render('create.ejs'))
-app.post('/', (req, res) => res.render('create.ejs', console.log(req.body)))
-
+app.use(express.static('public'));
+app.use('/css', express.static(`${__dirname}public/css`));
 
 
-
-
-
-
-
+app.get('/', (req, res) => res.render('create.ejs'));
+app.post('/', (req, res) => res.render('create.ejs', console.log(req.body)));
