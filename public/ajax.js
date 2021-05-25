12ee16727d2a458318036f77a34930349b0d0274
6a63p <fullname@iMac6a63p.local>
2021-05-25 14:47:31 +0300

    estlint

--- public/ajax.js
+++ public/ajax.js
@@ -1,21 +1,21 @@
 document.querySelector('button').addEventListener('click', () => {
-  if (document.getElementById('check').checked) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:5000');
-    xhr.setRequestHeader('Content-type', 'application/json');
+    if (document.getElementById('check').checked) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:5000');
+        xhr.setRequestHeader('Content-type', 'application/json');
 
-    xhr.send(JSON.stringify(
-      {
-        lox: document.querySelector('input').value,
-      },
-    ));
+        xhr.send(JSON.stringify(
+            {
+                lox: document.querySelector('input').value,
+            },
+        ));
 
-    xhr.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        console.log(this);
-      }
-    };
-  } else {
-    alert('Check please checkbox');
-  }
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log(this);
+            }
+        };
+    } else {
+        alert('Check please checkbox');
+    }
 });
